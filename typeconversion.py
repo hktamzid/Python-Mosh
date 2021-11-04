da8397879b276df8120f00be228f193fe87e7a2e
hktamzid <20mh1a0536@acoe.edu.in>
2021-11-04 22:14:16 +0530

    New code

---
+++ typeconversion.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+result1 = num1 + num2
+result2 = num1 - num2
+result3 = num1 * num2
+result4 = num1 / num2
+print("The summation is: ",result1)
+print("The substraction is: ",result2)
+print("The multiplication is: ",result3)
+print("The division is: ",result4)
+
+result5 = float(result1)
+print(result5)
