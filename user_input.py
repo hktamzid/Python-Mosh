f138ca3c9c09073522c2bce38689b462a0b392ae
hktamzid <20mh1a0536@acoe.edu.in>
2021-11-04 14:06:52 +0530

    New code

---
+++ user_input.py
@@ -0,0 +1,18 @@
+name = "Tamzid Mahmud"
+roll = "20MH1A0536"
+reg = "12345"
+name2 = "Sompa Akter"
+roll2 = "20MH1A0503"
+reg2 = "54321"
+
+print("Student Information")
+print("-------------------")
+print("Name: "+name)
+print("Roll No: "+roll)
+print("Reg No: "+reg)
+
+print("\n\nStudent Information 2")
+print("-------------------")
+print("Name: "+name2)
+print("Roll No: "+roll2)
+print("Reg No: "+reg2)
