2e19a6388ede25a0edf277c79d8a661dc360665a
hktamzid <20mh1a0536@acoe.edu.in>
2021-11-05 20:29:17 +0530

    new text

--- areaofshape.py
+++ areaofshape.py
@@ -3,11 +3,11 @@ h = float(input("Enter the height value: "))
 
 area = 0.5 * b * h
 
-print(area)'''
+print("Area of rectangle is: ",area)'''
 
 
 radius = float(input("Enter radius: "))
 
 area = 3.1416 * radius * radius
 
-print(area)
+print("Area of circle is: ",area)
