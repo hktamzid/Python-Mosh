7aa5edd9288a7b63dd30c7e4dbae1c80b389eef2
hktamzid <20mh1a0536@acoe.edu.in>
2021-11-05 21:42:37 +0530

    New prog

---
+++ formattedstring.py
@@ -0,0 +1,10 @@
+num1 = 30
+num2 = 20
+
+#bal = 20.23
+print(type(True))
+
+print(f"{num1} + {num2} = {num1 + num2}")
+print("My name is Tamzid Mahmud.", end=" ")
+print("My phone number is: 8096853590.", end=" ")
+print("My hondowa's name is Royal Enfield")
