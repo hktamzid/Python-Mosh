da5ea4e9b7593af1bd15ac6103fbeec875d0dab5
hktamzid <20mh1a0536@acoe.edu.in>
2021-11-05 20:27:03 +0530

    new program

---
+++ areaofshape.py
@@ -0,0 +1,13 @@
+'''b = float(input("Enter the base value: "))
+h = float(input("Enter the height value: "))
+
+area = 0.5 * b * h
+
+print(area)'''
+
+
+radius = float(input("Enter radius: "))
+
+area = 3.1416 * radius * radius
+
+print(area)
