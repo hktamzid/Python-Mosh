7f91c4e6ead9dbee11cdf9ceeb630956521eb5c0
hktamzid <20mh1a0536@acoe.edu.in>
2021-11-06 23:45:22 +0530

    New prog

---
+++ ifelsestatement.py
@@ -0,0 +1,25 @@
+mark = int(input("Enter your number: "))
+
+if mark>=90:
+    print("Congrats, Your grade is A+")
+elif mark>=80:
+    print("Congrats, your grade is A")
+elif mark>=70:
+    print("Congrats, your grade is B")
+elif mark>=60:
+    print("Congrats, your grade is C")
+elif mark>=50:
+    print("Congrats, your grade is D")
+elif mark>=40:
+    print("Congrats, your grade is E")
+if mark<30:
+    print("Alas! You have failed")
+
+'''
+num = int(input("Enter a ingeger number: "))
+
+if num%2==0:
+    print("Even")
+else:
+    print("Odd")
+'''
