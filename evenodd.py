1b73d50f67d1d0dc3710baa334d712ce27e1c415
hktamzid <20mh1a0536@acoe.edu.in>
2021-11-07 13:10:55 +0530

    New prog

---
+++ evenodd.py
@@ -0,0 +1,10 @@
+'''num = int(input("Enter the number: "))
+a = 2
+b = 5
+print("even(a),odd(b)")'''
+
+num = int(input("Enter a number: "))
+if num%2==0:
+    print("Even")
+else:
+    print("Odd")
